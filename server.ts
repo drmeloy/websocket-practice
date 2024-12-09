b974c993df12c1199dda22237e3b30ce5062d7ec
unknown <drmeloy@gmail.com>
2024-12-08 16:21:04 -0800

    socket.io initialization

--- server.ts
+++ server.ts
@@ -1,13 +1,25 @@
 import express from 'express';
 import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { Server } from 'socket.io';
 
 const app = express();
-const server = createServer(app);
+const httpServer = createServer(app);
+const socketServer = new Server(httpServer);
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get('/', (req, res) => {
-  res.send('<h1>Is there anybody out there...?</h1>');
+  res.sendFile(join(__dirname, 'example-index.html'));
 });
 
-server.listen(3333, () => {
+socketServer.on('connection', (socket) => {
+  console.log(`Socket ${socket.id} has connected`);
+  socket.on('disconnect', () => {
+    console.log(`Socket ${socket.id} has disconnected`)
+  })
+})
+
+httpServer.listen(3333, () => {
   console.log('Somebody is listening at http://localhost:3333');
 });
