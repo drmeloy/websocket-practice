7f456a3235d9048455d6b8d5ffff60e8a40622c0
unknown <drmeloy@gmail.com>
2024-12-08 16:07:01 -0800

    server start

--- server.ts
+++ server.ts
@@ -1,2 +1,13 @@
 import express from 'express';
 import { createServer } from 'node:http';
+
+const app = express();
+const server = createServer(app);
+
+app.get('/', (req, res) => {
+  res.send('<h1>Is there anybody out there...?</h1>');
+});
+
+server.listen(3333, () => {
+  console.log('Somebody is listening at http://localhost:3333');
+});
