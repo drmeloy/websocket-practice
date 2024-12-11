ac749f37af8903c90657310cd5582c1074d4778e
unknown <drmeloy@gmail.com>
2024-12-10 21:38:36 -0800

    adding game elements

---
+++ src/components/PlayerBoard.tsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+export const PlayerBoard = () => {
+  return (
+    <div className="player-board">
+      <div className="reaction-square">
+
+      </div>
+    </div>
+  )
+}
